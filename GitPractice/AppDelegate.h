dbc30452182a792c2a1a2c8482f012634811b551
rogerperez <rogerperez@u.boisestate.edu>
2014-02-12 13:44:45 -0700

    Initial Commit

---
+++ GitPractice/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitPractice
+//
+//  Created by Roger Perez on 2/12/14.
+//  Copyright (c) 2014 Roger Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitPractice/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitPractice
+//
+//  Created by Roger Perez on 2/12/14.
+//  Copyright (c) 2014 Roger Perez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
