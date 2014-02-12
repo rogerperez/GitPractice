8b4d28944fab9f76d3b49029a291a8aeed130b13
rogerperez <rogerperez@u.boisestate.edu>
2014-02-12 15:17:17 -0700

    Added the ability to change between images (crappy animation, stretched images)

--- GitPractice/ViewController.m
+++ GitPractice/ViewController.m
@@ -10,13 +10,28 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIImageView *startupImages;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
+    NSMutableArray *startupImages = [[NSMutableArray alloc] init];
+    for (int i = 1; i < 6; i++) {
+        UIImage *addedImage = [UIImage imageNamed:[NSString stringWithFormat:@"shot%d", i]];
+        [startupImages addObject:addedImage];
+    }
+
+    NSTimeInterval duration = 30;
+    self.startupImages.image = [UIImage animatedImageWithImages:startupImages duration:duration];
+    
+    
+    
+    //UIImage *animatedImage =
     [super viewDidLoad];
+    
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
